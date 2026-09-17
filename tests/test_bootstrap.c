9f9f679148a5dea4cf6cb6ce4abf70be561628a8
tiagooliveirajs <tiagooliveirajs@yahoo.com>
2026-09-17 03:54:55 +0100

    test(g05): make firmware version regression check active

--- tests/test_bootstrap.c
+++ tests/test_bootstrap.c
@@ -1,10 +1,8 @@
 #include "blu2usb/domain/version.h"
 
-#include <assert.h>
 #include <string.h>
 
 int main(void)
 {
-    assert(strcmp(blu2usb_version(), "0.3.0-g03") == 0);
-    return 0;
+    return strcmp(blu2usb_version(), "0.5.0-g05") == 0 ? 0 : 1;
 }
